6c26ebde3d97d049f5c6dc7026446cd30896a87c
choiinho <ino0319@gmail.com>
2020-07-31 23:29:18 +0900

    memberDAO

--- ex00/src/main/java/com/ino/persistence/MemberDAO.java
+++ ex00/src/main/java/com/ino/persistence/MemberDAO.java
@@ -3,9 +3,13 @@ package com.ino.persistence;
 import com.ino.domain.MemberVO;
 
 public interface MemberDAO {
-	
+
 	public String getTime();
-	
+
 	public void insertMember(MemberVO vo);
+
+	public MemberVO readMember(String userid) throws Exception;
+
+	public MemberVO readWithPW(String userid, String userpw) throws Exception;
 
 }
