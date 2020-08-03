ade7f40fd8641adf2664f6d170c01f6d8d5b8e2b
choiinho <ino0319@gmail.com>
2020-08-03 22:47:22 +0900

    impl

--- ex00/src/main/java/com/ino/persistence/MemberDAOImpl.java
+++ ex00/src/main/java/com/ino/persistence/MemberDAOImpl.java
@@ -1,5 +1,8 @@
 package com.ino.persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
 
 import org.apache.ibatis.session.SqlSession;
@@ -23,6 +26,22 @@ public class MemberDAOImpl implements MemberDAO {
 	@Override
 	public void insertMember(MemberVO vo) {
 		sqlSession.insert(namespace + ".insertMember", vo);
+	}
+
+	@Override
+	public MemberVO readMember(String userid) throws Exception {
+		return (MemberVO) sqlSession.selectOne(namespace + ".selectMember", userid);
+	}
+
+	@Override
+	public MemberVO readWithPW(String userid, String userpw) throws Exception {
+
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+
+		paramMap.put("userid", userid);
+		paramMap.put("userpw", userpw);
+
+		return sqlSession.selectOne(namespace + ".readWithPW", paramMap);
 	}
 
 }
